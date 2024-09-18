39e95c7c727d05c3f2c7b91553916bb2a7c75a0a
Enzo Soddano <enzosoddano1203@gmail.com>
2024-09-18 08:33:48 -0300

    Adicionar botão de navegação no About

--- projeto_router/src/pages/About/About.tsx
+++ projeto_router/src/pages/About/About.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './About.css'
 
 const AboutPage = () => {
@@ -5,7 +6,10 @@ const AboutPage = () => {
     return (
 
         <div>
+
             <h1>About Page</h1>
+            <Link to='/'>Go to Home</Link>
+
         </div>
 
     )
