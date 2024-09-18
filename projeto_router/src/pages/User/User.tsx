77b72048561f8b5217438030ee8c1dcd14b7b93c
Enzo Soddano <enzosoddano1203@gmail.com>
2024-09-18 08:52:38 -0300

    Adicionar UseParams em User.tsx

--- projeto_router/src/pages/User/User.tsx
+++ projeto_router/src/pages/User/User.tsx
@@ -1,13 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import './User.css'
 
 const UserPage = () => {
+
+    const { id } = useParams()
 
     return (
 
         <div>
 
             <h1>User Page</h1>
+            <p>User ID: {id}</p>
             <Link to='/'>Go to Home</Link>
 
         </div>
