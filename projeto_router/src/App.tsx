119b7c217db376dd06d316a9ff46ad26a5df1877
Enzo Soddano <enzosoddano1203@gmail.com>
2024-09-18 08:40:29 -0300

    Adicionar NotFoundPage na navegação do App.tsx

--- projeto_router/src/App.tsx
+++ projeto_router/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/Home/Home'
 import AboutPage from './pages/About/About'
+import NotFoundPage from './pages/NOTFOUND/NOTFOUND'
 
 function App() {
   
@@ -13,7 +14,7 @@ function App() {
 
         <Route path='/' element={<HomePage />} />
         <Route path='/about' element={<AboutPage />} />
-        <Route path='*' element={<HomePage />} />
+        <Route path='*' element={<NotFoundPage />} />
 
       </Routes>
 
