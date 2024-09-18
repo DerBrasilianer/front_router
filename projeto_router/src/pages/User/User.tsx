f605d5531cf5e8c2d04ff858438172e89cf62dab
Enzo Soddano <enzosoddano1203@gmail.com>
2024-09-18 08:47:17 -0300

    Código de User.tsx

---
+++ projeto_router/src/pages/User/User.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import './User.css'
+
+const UserPage = () => {
+
+    return (
+
+        <div>
+
+            <h1>User Page</h1>
+            <Link to='/'>Go to Home</Link>
+
+        </div>
+
+    )
+
+}
+
+export default UserPage
