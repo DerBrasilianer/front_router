4ca6cfdc737a7607426788a0df6deab46d035cc6
Enzo Soddano <enzosoddano1203@gmail.com>
2024-09-18 08:44:00 -0300

    Adicionar useNavigate em NOTFOUND.tsx

--- projeto_router/src/pages/NOTFOUND/NOTFOUND.tsx
+++ projeto_router/src/pages/NOTFOUND/NOTFOUND.tsx
@@ -1,14 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import './NOTFOUND.css'
 
 const NotFoundPage = () => {
+
+    const navigate = useNavigate()
+
+    const toHomePage = () => {
+
+        console.error('Redirecting 404 to Home Page')
+        navigate('/')
+
+    }
 
     return (
 
         <div>
 
             <h1>Not Found - 404</h1>
-            <Link to='/'>Go to Home</Link>
+            <button onClick={toHomePage}>Go to Home</button>
 
         </div>
 
