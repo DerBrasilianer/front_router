f52fcf153f9398d52b3cac46d4cbb1c3e8fefb9b
Enzo Soddano <enzosoddano1203@gmail.com>
2024-09-18 09:04:02 -0300

    Adicionar redirecionamento para 404 se id for >5

--- projeto_router/src/pages/User/User.tsx
+++ projeto_router/src/pages/User/User.tsx
@@ -1,16 +1,35 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import './User.css'
+import { useEffect } from "react";
 
 const UserPage = () => {
 
     const { id } = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+
+        if (id && parseInt(id) > 5) {
+
+            console.error('Redirecting User Page to 404')
+            navigate('/404')
+
+        }
+
+    }, [id, navigate])
+
+    if (!id) {
+
+        return null
+
+    }
 
     return (
 
         <div>
 
             <h1>User Page</h1>
-            <p>User ID: {id}</p>
+            <p>User ID: {parseInt(id)}</p>
             <Link to='/'>Go to Home</Link>
 
         </div>
