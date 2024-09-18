67897e5cd705b93c5432e4edf8a6e3b85b9f2bdf
Enzo Soddano <enzosoddano1203@gmail.com>
2024-09-18 08:38:29 -0300

    Código de NOTFOUNDPAGE.tsx

---
+++ projeto_router/src/pages/NOTFOUND/NOTFOUND.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import './NOTFOUND.css'
+
+const NotFoundPage = () => {
+
+    return (
+
+        <div>
+
+            <h1>Not Found - 404</h1>
+            <Link to='/'>Go to Home</Link>
+
+        </div>
+
+    )
+
+}
+
+export default NotFoundPage
